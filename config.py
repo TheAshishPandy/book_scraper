cf9bd62024cd9972955ac3f93577087672fd843d
Ashish Pandey <Ashish.pandey@businessnext.com>
2025-06-19 10:11:29 +0530

    changes in the tsl for the book

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 BASE_URL   = "http://books.toscrape.com/catalogue/"
 START_PAGE = "page-1.html"
 HEADERS    = {"User-Agent": "Mozilla/5.0"}
-MONGO_URI  = "mongodb+srv://theashishpandy:xEG0i2luLToOkxCv@books.kustpvh.mongodb.net/?retryWrites=true&w=majority&appName=books"
+MONGO_URI = "mongodb+srv://theashishpandy:xEG0i2luLToOkxCv@books.kustpvh.mongodb.net/?retryWrites=true&w=majority&tls=true&appName=books"
+
+
 
 CATEGORY_SLUGS = {
     'Travel':              'travel_2',
